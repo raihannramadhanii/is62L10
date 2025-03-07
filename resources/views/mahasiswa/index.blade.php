71ea465c42a0081c87b66199552ab898afd42c2f
raihannramadhanii <raihannramadhanii@gmail.com>
2025-03-07 11:55:54 +0700

    blade

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -22,14 +23,15 @@
     }
     ?>
     </ol>
-    {{--
+    {{--dfdgf
     <ol>
         <li> Khalifah </li>
         <li> Herdio </li>
         <li> Badri </li>
         <li> Raihan </li>
         <li> Putri </li>
-    </ol>
-    --}}
+    </ol> --}}
+    <script src="{{ asset('/js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('/js/bootstrap.js') }}"></script>
 </body>
 </html>
