72af55bea70b6146705e82ff297c72901d04e1cb
putrafahrezi <raihannramadhanii@gmail.com>
2025-04-25 10:50:48 +0700

    migration mahasiswas dan dosens

---
+++ database/migrations/2025_04_25_032115_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim')->unique();
+            $table->string('nama',100);
+            $table->string('tempat_lahir',100);
+            $table->date('tanggal_lahir');
+            $table->bigInteger('dosens_id');
+            $table->enum('jk', ['laki-laki', 'perempuan']);
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
