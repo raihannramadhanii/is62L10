d744f2ec317d6b8b6b1ff4bfdf314a6296fc615d
putrafahrezi <raihannramadhanii@gmail.com>
2025-05-09 11:55:20 +0700

    membuat table data dosen dan menambahkan button tambah data

--- resources/views/Dosen/index.blade.php
+++ resources/views/Dosen/index.blade.php
@@ -5,10 +5,48 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h3 class="fw-bold">{{ __('Data Dosen') }}</h3>
+                    <a href="form.blade.php" class="btn btn-primary">Tambah Data</a>
+                </div>
 
                 <div class="card-body">
-                    INI AKAN MENAMPILKAN DATA DOSEN
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">NIDN</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>67348634</td>
+                                <td>Maulian Saputra</td>
+                                <td>mauliansaputra@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>8676538</td>
+                                <td>Luthfi</td>
+                                <td>luthfi@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>5655767</td>
+                                <td>Hadi Syafputra</td>
+                                <td>hadisyaf@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>78967867</td>
+                                <td>Susi Yahya</td>
+                                <td>susiyahya@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
